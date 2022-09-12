37a57b8f4409a2d7a1f7df08dec435d77db4ed4a
nicolas1897 <106556299+nicolas1897@users.noreply.github.com>
2022-09-12 11:06:10 -0500

    fixing betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,137 +1,108 @@
 #include "sort.h"
+#include <stdlib.h>
+void sort(int *array, size_t beg, size_t size, size_t);
+int check(int *array, size_t beg, size_t end);
+int swap(int *, size_t, size_t, size_t);
 
 /**
-
- * partition - partitions the array
-
- * @array: array to take in
-
- * @start: start of array;
-
- * @end: end of array
-
- * @size: full size of array
-
- * Return: position of pivot
-
+ * quick_sort - Sorts an array of integers in asceding order using the
+ * quick sort algorithm implementing the Lomuto partition scheme.
+ * @array: Array of integers that need to be sorted.
+ * @size: The size of the array to sort.
  */
-
-int partition(int *array, int start, int end, int size)
-  
-{
-  
-  int pivot = array[end];
-  
-  int i = start, j, temp;
-  
-
-  
-  for (j = start; j < end; j++)
-    
-    {
-      
-      if (array[j] <= pivot)
-	
-	{
-	  
-	  if (i != j)
-	    
-	    {
-	      
-	      temp = array[i];
-	      
-	      array[i] = array[j];
-	      
-	      array[j] = temp;
-	      
-	      print_array(array, size);
-	      
-	    }
-	  
-	  i++;
-	  
-	}
-      
-    }
-  
-  if (i != end)
-    
-    {
-      
-      temp = array[i];
-      
-      array[i] = array[end];
-      
-      array[end] = temp;
-      
-      print_array(array, size);
-      
-    }
-  
-  printf("return i=%d\n", i);
-  
-  return (i);
-  
-}
-
-/**
-
- * quickSort - quick sorts with recursion
-
- * @array: array to sort through
-
- * @start: start of array or subarray
-
- * @end: end of array or subarray
-
- * @size: size of full array
-
- */
-
-void quickSort(int *array, int start, int end, int size)
-  
-{
-  
-  int pivot;
-  
-
-  
-  if (start < end)
-    
-    {
-      
-      pivot = partition(array, start, end, size);
-      
-      printf("first recursive, start [%d] to pivot-1[%d]\n", start, pivot-1);
-      
-      quickSort(array, start, pivot - 1, size);
-      
-      printf("second recursive, pivot+1 [%d] to end [%d]\n", pivot+1, end);
-      
-      quickSort(array, pivot + 1, end, size);
-      
-    }
-  
-}
-
-/**
-
- * quick_sort - quick sorts an array
-
- * @array: array to sort
-
- * @size: size of array
-
- */
-
 void quick_sort(int *array, size_t size)
-  
 {
-  
-  if (array == NULL || size < 2)
-    
-    return;
-  
-  quickSort(array, 0, size - 1, size);
-  
+	size_t beg;
+
+	beg = 0;
+	if (size < 2 || array == NULL)
+		return;
+	size = size - 1;
+	sort(array, beg, size, size + 1);
+}
+
+/**
+ * sort - Does the actual sorting.
+ * @array: Array of integers that need to be sorted.
+ * @beg: The lower bound of the array partition.
+ * @end: The upper bound of the array partion.
+ * @og: The original size of the whole array. Used only for printing.
+ */
+void sort(int *array, size_t beg, size_t end, size_t og)
+{
+	size_t i;
+	size_t j;
+	int piv;
+	int flag;
+	int c;
+
+	piv = array[end];
+	j = beg;
+	i = j - 1;
+	flag = 0;
+
+	for (j = beg; j < end; j++)
+	{
+		if (array[j] <= piv)
+		{
+			i++;
+			flag = swap(array, j, i, og);
+		}
+	}
+	i++;
+	flag = swap(array, j, i, og);
+
+	if (flag == 0)
+	{
+		c = check(array, beg, end);
+		if (c == 1)
+			sort(array, beg, end - 1, og);
+	}
+	else
+	{
+		sort(array, beg, i, og);
+		sort(array, i, end, og);
+	}
+}
+
+/**
+ * swap - Swaps the numbers in the array.
+ * @array: Array of integers that need to be sorted.
+ * @i: The lower bound of the array partition.
+ * @j: The upper bound of the array partion.
+ * @og: The original size of the whole array. Used only for printing.
+ * Return: 1 when a swap happens. 0 otherwise.
+ */
+int swap(int *array, size_t i, size_t j, size_t og)
+{
+	int tmp;
+
+	if (j != i)
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+		print_array(array, og);
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * check - checks if the array portion is already sorted.
+ * @array: Array of integers that need to be sorted.
+ * @beg: The lower bound of the array partition.
+ * @end: The upper bound of the array partion.
+ * Return: 1 if the array is not sorted. 0 otherwise.
+ */
+int check(int *array, size_t beg, size_t end)
+{
+	size_t i;
+
+	for (i = beg; i < end; i++)
+	{
+		if (array[i] > array[i + 1])
+			return (1);
+	}
+	return (0);
 }
